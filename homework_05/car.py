dba5495d5ba35a1e82c24b3726bde8504e31e2a1
OsipovDN <disorder_2@list.ru>
2026-03-28 20:45:50 +0300

    add homework 5

--- homework_05/car.py
+++ homework_05/car.py
@@ -1,3 +1,13 @@
 """
 Создайте класс `Car`, наследник `Vehicle`
 """
+
+from homework_05.base import Vehicle
+from homework_05.engine import Engine
+
+class Car(Vehicle):
+    engine: Engine
+
+    @engine.setter
+    def engine(self, engine: Engine) -> None:
+        self.engine = self.engine
--- homework_05/plane.py
+++ homework_05/plane.py
@@ -1,3 +1,21 @@
 """
 Создайте класс `Plane`, наследник `Vehicle`
 """
+from homework_05.base import Vehicle
+from homework_05.exceptions import CargoOverload
+
+
+class Plane(Vehicle):
+    cargo: float
+    max_cargo: float
+
+    
+    def __init__(self, max_cargo: float):
+        super().__init__()
+
+
+    def load_cargo(self, cargo: float):
+        if (self.cargo + cargo) > self.max_cargo:
+            raise CargoOverload
+        else:
+            self.cargo += cargo
